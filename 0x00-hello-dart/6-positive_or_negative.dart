42223bd6ffee8e4cde0dde4ebd447d1a8fdf6cb4
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2023-11-10 12:28:23 +0100

    Positive or Negative

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,11 @@
+// + or -
+void main (List<String> args) {
+  var number = int.parse(args[0]);
+  if (number > 0) {
+    print("${number} is positive");
+  } else if (number < 0) {
+    print("${number} is negative");
+  } else {
+    print("${number} is zero");
+  }
+}
