9f1ba068fbec7dfb6f32f2fd240d9b7abeba8f65
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2023-11-17 11:37:06 +0100

    str

--- 0x01-dart-function/1-concat_strings.dart
+++ 0x01-dart-function/1-concat_strings.dart
@@ -1,4 +1,4 @@
 // a  function  that accepts two arguments.
-String concatStr(String str1, String str2) {
-return('${str1} ${str2}')
+String concatStr(String str1, String str2){
+return"${str1} ${str2}";
 }
