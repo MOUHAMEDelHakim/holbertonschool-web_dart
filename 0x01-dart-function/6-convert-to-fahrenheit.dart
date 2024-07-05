05d18e92de1e63c91953b6cd0fc7a1d9302aa77d
MOUHAMEDelHakim <mohamedelhakim7@gmail.com>
2024-07-05 12:03:40 +0100

    converts

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,5 +1,6 @@
 // ConcertTof
-List<double> convertToF(List<double> temperaturesInC);
+List<double> convertToF(List<double> temperaturesInC){
 def convertToF(temperaturesInC):
     temperaturesInF = [(temp * 9/5) + 32 for temp in temperaturesInC]
     return temperaturesInF
+}
