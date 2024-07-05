d30ac711c88ac0d0b304a85e572323c902654c17
MOUHAMEDelHakim <mohamedelhakim7@gmail.com>
2024-07-05 12:19:47 +0100

    longestUniqueSubstring

---
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -0,0 +1,17 @@
+String longestUniqueSubstring(String str) {
+  var tmp = str.split('');
+  var res = '';
+  var longest = '';
+
+  tmp.forEach((l) {
+    if (res.contains(l)) {
+      longest = longest.length < res.length ? res : longest;
+      res = '';
+    }
+    res += l;
+  });
+
+  longest = longest.length < res.length ? res : longest;
+  
+  return longest;
+}
