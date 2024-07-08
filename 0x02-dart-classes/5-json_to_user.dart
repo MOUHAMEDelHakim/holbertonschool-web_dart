ea73de5d0bb0ff7315117798dadaf3c333ae5700
MOUHAMEDelHakim <mohamedelhakim7@gmail.com>
2024-07-08 13:31:34 +0100

    json

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,40 +1,28 @@
+// user to jason.
 class User {
-  int? id;
-  String? name;
-  int? age;
-  double? height;
+  String name;
+  int age;
+  double height;
+  int id;
 
-  User({this.id, this.name, this.age, this.height});
+  User({
+    required this.name,
+    required this.age,
+    required this.height,
+    required this.id,
+  });
 
-  // Static method to create a User instance from JSON
-  static User fromJson(Map<dynamic, dynamic> userJson) {
-    return User(
-      id: userJson['id'],
-      name: userJson['name'],
-      age: userJson['age'],
-      height: userJson['height'],
-    );
-  }
-
-  // Method to return a string representation of the User instance
-  @override
+  Map toJson() {
+    return {
+      'name': name,
+      'age': age,
+      'height': height,
+      'id' : id,
+    };
+    }
+    @override
   String toString() {
-    return 'User(id: $id, name: $name, age: $age, height: $height)';
+    // TODO: implement toString
+    return super.toString();
   }
-}
-
-void main() {
-  // Example JSON data
-  Map<dynamic, dynamic> userJson = {
-    'id': 1,
-    'name': 'Alice',
-    'age': 30,
-    'height': 5.5,
-  };
-
-  // Create a User instance from JSON
-  User user = User.fromJson(userJson);
-
-  // Print the string representation of the User instance
-  print(user); // Output: User(id: 1, name: Alice, age: 30, height: 5.5)
-}
+  }
