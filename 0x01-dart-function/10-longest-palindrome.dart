41821c4118446fdd7062d55cb1515f7911b482d1
MOUHAMEDelHakim <mohamedelhakim7@gmail.com>
2024-07-08 10:43:32 +0100

    polindrome

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,4 +1,27 @@
-String longestPalindrome(String s);
-if (s.length < 3) {
-    return "none";
+String longestPalindrome(String s) {
+  if (s.length < 3) return "none";
+
+  bool isPalindrome(String str) {
+    int left = 0;
+    int right = str.length - 1;
+    while (left < right) {
+      if (str[left] != str[right]) return false;
+      left++;
+      right--;
+    }
+    return true;
+  }
+
+  String longest = "none";
+
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i + 3; j <= s.length; j++) {
+      String substr = s.substring(i, j);
+      if (isPalindrome(substr) && substr.length > longest.length) {
+        longest = substr;
+      }
+    }
+  }
+
+  return longest;
 }
